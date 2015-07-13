b8577661bdb752a925026d2e1671921db81e0265
Zachary Suffern <zsuffern0614@gatech.edu>
2015-07-13 14:03:37 -0600

    Converting to angular

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # loc
 Library of Congress - Chronicling America Project
+
+
+
+
+If getting an error saying you cannot find a tile.png object:
+	On line 1875 of Angular-Leaflet-Directive.js, needs to look something like this:
+            tileLayer: 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',       
+	if it is missing the http: then the app will break. Min does not have this so make sure to use the regular .js file. 
--- map.js
+++ map.js
@@ -1,14 +1,56 @@
-document.body.innerHTML += '<div id="map" ng-app="myApp" ng-controller="customersCtrl"></div>';
-document.getElementById("map").style.height = "500px";
-document.body.innerHTML +=  '<input type="text" id="amount" size="100" readonly style="border:0; color:#f6931f; font-weight:bold;">'
+var app = angular.module("myApp", ['leaflet-directive']);
+app.controller("MapCtrl", [ "$scope","$http","leafletData", "leafletBoundsHelpers", function($scope, $http, leafletData, leafletBoundsHelpers) {
+    
 
-document.body.innerHTML += '<div id="slider"></div>';
-document.body.innerHTML += '<p>Date from <input type="text" id="datepicker1"> to <input type="text" id="datepicker2"> </p>'
-document.body.innerHTML += '<p>Search for <input type="text" id="searchterm">     <input type="submit" value="Search"></p>';
+    var bounds = leafletBoundsHelpers.createBoundsFromArray([
+        [ 24.11567, -125.73004 ],
+        [50.38407, -65.94975 ],
+    ]);
+
+     var tiles = {
+        url: "https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}",
+        options: {
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+            maxZoom: 18,
+            id: 'zsuffern0614.2ed6b495',
+            accessToken: 'pk.eyJ1IjoienN1ZmZlcm4wNjE0IiwiYSI6IjVlNWFkYjQwZDc0ZTY0OTZmMDQyMzM4NmVmMjFmNWNiIn0.oZhSA6w9Pgv3ISwLjP7vTQ'
+        }
+    }
+
+    var markers = {};
+
+    $http.get("http://130.207.211.77:8983/solr/loc/select?q=date_field%3A%5B1914-03-06T23%3A59%3A59.999Z+TO+1914-03-07T23%3A59%3A59.999Z%5D%2C+text%3A%22christmas%22&rows=100&wt=json&indent=true")
+    .success(function (response){
+        for (i = 0; i < response.response.docs.length; i++) { 
+            var datum = response.response.docs[i];
+            var loc = datum.loc.split(',');
+            var mark = {
+                lat:parseFloat(loc[0]),
+                lng:parseFloat(loc[1]),
+                message: "<b>" + datum.city + "," + datum.state+"</b><br>"+datum.seq_num
 
 
-        
+            }
+            // mark.bindPopup("<b>" + datum.city + "," + datum.state+"</b><br>"+datum.seq_num);
+            console.log(mark);
+            markers[i] = mark;
+        }
+    });
 
+
+    angular.extend($scope, {
+        maxbounds: bounds,
+        center: {},
+        tiles: tiles,
+        markers:markers
+    });
+}]);
+
+
+
+
+
+/*document.getElementById("map").style.height = "500px";//style through css
 
 var maxBounds = [
     [24.11567, -125.73004], //Southwest
@@ -32,18 +74,25 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 // marker.bindPopup("<b>Hello world!</b><br>I am a popup.");
 // date_field:[1914-03-06T23:59:59.999Z TO 1914-03-07T23:59:59.999Z]
 // goes from 1836 - 1922 , 36,890 days difference
+
+
+/*
 var app = angular.module('myApp', []);
-app.controller('customersCtrl', function($scope, $http) {
+app.controller('MapCtrl', function($scope, $http) {
     $http.get("http://130.207.211.77:8983/solr/loc/select?q=date_field%3A%5B1914-03-06T23%3A59%3A59.999Z+TO+1914-03-07T23%3A59%3A59.999Z%5D%2C+text%3A%22christmas%22&rows=100&wt=json&indent=true")
     .success(function (response){console.log(response.response);
-    for (i = 0; i < response.response.docs.length; i++) { 
-        var datum = response.response.docs[i];
-        var loc = datum.loc.split(',');
-        var mark = L.marker([parseFloat(loc[0]),parseFloat(loc[1])]).addTo(map);
-        mark.bindPopup("<b>" + datum.city + "," + datum.state+"</b><br>"+datum.seq_num);
+        for (i = 0; i < response.response.docs.length; i++) { 
+            var datum = response.response.docs[i];
+            var loc = datum.loc.split(',');
+            var mark = L.marker([parseFloat(loc[0]),parseFloat(loc[1])]).addTo(map);
+            mark.bindPopup("<b>" + datum.city + "," + datum.state+"</b><br>"+datum.seq_num);
+        }
+    });
+});*/
 
-    }
 
+//app.controller('searchCtrl', function($scope, $http) {});
+/*
 $(function() {
     $( "#slider" ).slider({ 
         range: true,
@@ -90,7 +139,7 @@ $(function() {
 
 
 
-  });
-});
+  });*/
+
 
  
