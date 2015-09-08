00bb5a178a7de212ba55c3cc1f8a886e44a9d73c
root <root@bisiprod001.gtri.org>
2015-09-08 15:28:29 -0400

    extra files added

--- python/server.py
+++ python/server.py
@@ -18,5 +18,17 @@ application = Flask(__name__)
 def home():
 	return "hello"
 
+@application.route('/get_data', methods=['GET', 'POST'])
+def milk():
+#	url = 'http://130.207.211.77:8983/solr/loc/select?q=date_field%3A%5B1836-01-02T00%3A00%3A00%3A000Z+TO+1925-01-01T00%3A00%3A00%3A000Z%5D+%0Atext%3A%22lincoln%22&wt=json&rows=1000&indent=true&fl=loc,date_field,id,city,state,ed,seq,seq_num'
+#	r = requests.get(url)
+	if request.method == 'POST':	
+		return request
+	else:
+		return "get milk"
+
+
+
+
 if __name__ == '__main__':
     application.run(debug=True,host='0.0.0.0',port=80)
