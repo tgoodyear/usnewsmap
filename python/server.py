05e7b74d8c7ea9bb417b022428b59419cc1a61e8
root <root@bisiprod001.gtri.org>
2015-09-09 15:52:19 -0400

    work on flask server

--- python/server.py
+++ python/server.py
@@ -4,6 +4,7 @@ import logging
 import sys
 import traceback
 import simplejson as json
+import datetime
 from flask import Flask, request, jsonify, Response
 from flask.ext.cors import CORS
 from flask.ext.restplus import Api, Resource, fields, apidoc
@@ -18,11 +19,23 @@ application = Flask(__name__)
 def home():
 	return "hello"
 
-@application.route('/get_data')
+@application.route('/get_data', methods=['GET', 'POST'])
 def milk():
-    url = 'http://130.207.211.77:8983/solr/loc/select?q=date_field%3A%5B1836-01-02T00%3A00%3A00%3A000Z+TO+1925-01-01T00%3A00%3A00%3A000Z%5D+%0Atext%3A%22lincoln%22&wt=json&rows=1000&indent=true&fl=loc,date_field,id,city,state,ed,seq,seq_num'
-	r = requests.get(url)
-	return r.json
+	if request.method == 'POST':
+		url = request.data
+        	r = requests.get(url)
+		data = json.loads(r.text)
+		for d in data['response']['docs']:
+			loc = d['loc'].split(",")
+			dats = map(int, d['date_field'].split("T")[0].split("-"))
+			date = datetime.date(dats[0],dats[1],dats[2])
+			print date
+		return r.text
+	else:
+		return "git milk"
+
+
+
 
 if __name__ == '__main__':
     application.run(debug=True,host='0.0.0.0',port=80)
